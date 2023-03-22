c6069c6d287f10b097d6db27fe455d7f8d70272d
Daniel Lee <jhoon8903@gmail.com>
2023-03-23 00:56:29 +0900

    tutorial Dart commit

---
+++ tutorial.dart
@@ -0,0 +1,36 @@
+// Main function은 항상 필요하다
+// void main() {
+//   print("Hello world");
+// }
+
+//----------------------------------------------------------------
+
+// var Keyword
+// import 'dart:ffi';
+// import 'dart:svg';
+
+//----------------------------------------------------------------
+
+// void main() {
+//   var name = true;
+//   // name = 1;
+//   // name = true;
+//   // name = 'Dart';
+//   // name = 2;
+//   name = false;
+// }
+
+//----------------------------------------------------------------
+
+// Dynamic Variable
+// void main() {
+//   dynamic name;
+//   if (name is String) {}
+// }
+
+//----------------------------------------------------------------
+// Null Safety
+
+void main() {
+  print("hEllo world is null");
+}
